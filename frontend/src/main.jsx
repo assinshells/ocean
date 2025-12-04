ec1312e563a2846610a6e379edc2240a67501ea5
Erik Thompson <122100036+assinshells@users.noreply.github.com>
2025-12-04 17:48:25 +0200

    modified:   frontend/src/main.jsx 	modified:   frontend/src/styles/app.css

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,6 +4,7 @@ import App from './App.jsx';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import './styles/main.css';
+import './styles/app.css';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
